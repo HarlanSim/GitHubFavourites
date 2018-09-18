7f98a00264672069f2229a6bdbc59fd7d23dd292
Harlan Sim <harlansim@gmail.com>
2018-09-17 21:47:30 -0400

    modify modal so that it only has a title, two panels

--- src/Modal.js
+++ src/Modal.js
@@ -2,20 +2,23 @@ import React, { Component } from 'react';
 
 class Modal extends Component {
   render() {
+    const {title, leftPanel, rightPanel} = this.props;
+
     return (
         <div className="modal show">
             <div className="modal-dialog">
                 <div className="modal-content">
                     <div className="modal-header">
                         <button type="button" className="close" data-dismiss="modal" aria-hidden="true">&times;</button>       
-                        <h4 className="modal-title">Modal title</h4>
+                        <h4 className="modal-title">{title}</h4>
                     </div>
                     <div className="modal-body">
-                        <p>One fine body&hellip;</p>
-                    </div>
-                    <div className="modal-footer">
-                        <button type="button" className="btn btn-default" data-dismiss="modal">Close</button>
-                        <button type="button" className="btn btn-primary">Save changes</button>
+                        <div>
+                            {leftPanel}
+                        </div> 
+                        <div>
+                            {rightPanel}
+                        </div>
                     </div>
                 </div>
             </div>
